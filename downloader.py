47b8d4d02b0111fa6a152ee28a914cf31bdd98eb
MrPotatoXx <salinasgiovanni25@gmail.com>
2023-01-08 22:37:35 -0300

    Creation of app to download audios or videos

---
+++ downloader.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+from PyQt5 import QtWidgets
+
+class Downloader(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.url_label = QtWidgets.QLabel("URL:", self)
+        self.url_input = QtWidgets.QLineEdit(self)
+        self.format_label = QtWidgets.QLabel("Formato:", self)
+        self.format_combo = QtWidgets.QComboBox(self)
+        self.format_combo.addItems(["Audio", "Video"])
+        self.download_button = QtWidgets.QPushButton("Descargar", self)
+        self.path_label = QtWidgets.QLabel("Ruta de descarga:", self)
+        self.path_input = QtWidgets.QLineEdit(self)
+        self.path_button = QtWidgets.QPushButton("...", self)
+
+        vbox = QtWidgets.QVBoxLayout()
+
+        vbox.addWidget(self.url_label)
+        vbox.addWidget(self.url_input)
+        vbox.addWidget(self.format_label)
+        vbox.addWidget(self.format_combo)
+
+        hbox = QtWidgets.QHBoxLayout()
+
+        hbox.addWidget(self.path_label)
+        hbox.addWidget(self.path_input)
+        hbox.addWidget(self.path_button)
+        hbox.addWidget(self.download_button)
+
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.download_button.clicked.connect(self.download)
+        self.path_button.clicked.connect(self.selectPath)
+
+        self.show()
+
+    def download(self):
+
+        url = self.url_input.text()
+        fmt = self.format_combo.currentText()
+
+        if fmt == "Audio":
+            fmt = "bestaudio"
+        elif fmt == "Video":
+            fmt = "best"
+
+        path = self.path_input.text()
+
+        if not path:
+            path = "."
+
+        result = subprocess.run(["yt-dlp", "--format", fmt, "--output", os.path.join(path, "%(title)s.%(ext)s"), url])
+
+        if result.returncode == 0:
+            QtWidgets.QMessageBox.information(self, "Descarga completada", "El archivo se ha descargado correctamente")
+        else:
+            QtWidgets.QMessageBox.critical(self, "Error", "Ocurrió un error durante la descarga")
+
+    def selectPath(self):
+        path = QtWidgets.QFileDialog.getExistingDirectory(self, "Seleccionar carpeta de descarga")
+
+        if path:
+            self.path_input.setText(path)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = Downloader()
+
+    app.exec_()
